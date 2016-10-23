1ba4387b424bb6ac1904633def9b6173ff4a60eb
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2016-10-23 19:52:12 +0300

    Initial commit. Add Mongo Provider

---
+++ Sources/App/main.swift
@@ -0,0 +1,20 @@
+import Vapor
+import VaporMongo
+
+let drop = Droplet()
+
+do {
+    try drop.addProvider(VaporMongo.Provider.self)
+} catch {
+    print(error)
+}
+
+drop.get { req in
+    return try drop.view.make("welcome", [
+    	"message": drop.localization[req.lang, "welcome", "title"]
+    ])
+}
+
+//drop.resource("posts", PostController())
+
+drop.run()
