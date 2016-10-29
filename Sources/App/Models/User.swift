7752b3fb6ef1508f34ce13ddddfae9bb6490caaf
Anton Poltoratskyi <anton.poltoratskii@gmail.com>
2016-10-29 21:24:56 +0300

    Add User model

---
+++ Sources/App/Models/User.swift
@@ -0,0 +1,77 @@
+//
+//  User.swift
+//  AppServer
+//
+//  Created by Anton Poltoratskyi on 29.10.16.
+//
+//
+
+
+import Foundation
+import Vapor
+import Auth
+
+final class User: Model {
+
+    var id: Node?
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    init(node: Node, in context: Context) throws {
+        id = try node.extract("id")
+        name = try node.extract("name")
+    }
+    
+    func makeNode(context: Context) throws -> Node {
+        return try Node(node: [
+            "id": id,
+            "name": name
+            ])
+    }
+    
+    static func prepare(_ database: Database) throws {
+        try database.create("users") { users in
+            users.id()
+            users.string("name")
+        }
+    }
+    
+    static func revert(_ database: Database) throws {
+        try database.delete("users")
+    }
+}
+
+
+extension User: Auth.User {
+    
+    static func authenticate(credentials: Credentials) throws -> Auth.User {
+        
+        let user: User?
+        
+        switch credentials {
+            
+        case let id as Identifier:
+            user = try User.find(id.id)
+        case let accessToken as AccessToken:
+            user = try User.query().filter("access_token", accessToken.string).first()
+        case let apiKey as APIKey:
+            user = try User.query().filter("email", apiKey.id).filter("password", apiKey.secret).first()
+        default:
+            break
+        }
+        
+        guard let authorizedUser = user else {
+            throw Abort.custom(status: .badRequest, message: "User not found.")
+        }
+        
+        return authorizedUser
+    }
+    
+    static func register(credentials: Credentials) throws -> Auth.User {
+        throw Abort.custom(status: .badRequest, message: "Registration not supported")
+    }
+}
+
